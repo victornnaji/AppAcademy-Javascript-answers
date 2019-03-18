4f8dd97e7e27a10bb785e6a6adc73aef3abef149
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-18 17:13:02 +0100

    Added the answer to the question 1

---
+++ Question 1/abbreviate_sentence.js
@@ -0,0 +1,10 @@
+function abbreviate_sentence(name){
+let arr = name.split(' ').map(x => capWord(x)).join(' ');
+
+return arr;
+}
+
+//function to capitalise word by word
+function capWord(word){
+    return word.toLowerCase().replace(word[0], word[0].toUpperCase());
+}
