7ab2f136e10146f858a5c42994d6dd6796e4dc9d
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-18 17:52:10 +0100

    added the answer in javascript

--- Question 1/to_initial.js
+++ Question 1/to_initial.js
@@ -1,5 +1,5 @@
 function to_initial(name){
-    let initial = name.toUpperCase().split(' ').map(x => x[0]).join('.');
+    let theInitial = name.toUpperCase().split(' ').map(x => x[0]).join('.');
 
-    return initial;
+    return theInitial;
 }
