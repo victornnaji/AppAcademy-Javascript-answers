d02f86b1ebf8013c78b0af1f0b9e20d92f3a8737
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-18 18:01:04 +0100

    added the answer in ruby

--- Question 2/first_in_array.rb
+++ Question 2/first_in_array.rb
@@ -7,4 +7,4 @@ def first_in_array(arr, el1, el2)
   end
   
   puts first_in_array(["a", "b", "c", "d"], "d", "b"); # => "b"
-  puts first_in_array(["cat", "bird" ,"dog", "mouse" ], "dog", "mouse"); # => "dog"
+  puts first_in_array(["cat", "bird" ,"dog", "mouse" ], "mouse", "dog"); # => "dog"
