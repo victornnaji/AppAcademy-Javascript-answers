c4ce219044e4ca90df1b1e082401176901156077
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-21 00:37:39 +0100

    answer in ruby

---
+++ Question 9/rotate_array.rb
@@ -0,0 +1,14 @@
+def rotate_array(arr, num)
+    num.times do
+      ele = arr.pop
+      arr.unshift(ele)
+    end
+  
+    return arr
+  end
+  
+  print rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
+  puts
+  
+  print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
+  puts
