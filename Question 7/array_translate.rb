63ee2c8aff0f1ec712594f1d8635cea078eece40
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-20 22:14:28 +0100

    answer in ruby

---
+++ Question 7/array_translate.rb
@@ -0,0 +1,17 @@
+def array_translate(array)
+  str = ""
+
+  i = 0
+  while i < array.length
+    ele = array[i]
+    num = array[i + 1]
+    num.times { str += ele }
+
+    i += 2
+  end
+
+  return str
+end
+
+print array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]); # => "CatCatDogDogDogMouse"
+puts
