90ba561d274c1930b97240d6ad34d606908e7ef3
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-21 17:05:42 +0100

    answer in ruby

---
+++ Question 11/combination.rb
@@ -0,0 +1,19 @@
+def combinations(arr)
+    pairs = []
+  
+    arr.each_with_index do |ele1, idx1|
+      arr.each_with_index do |ele2, idx2|
+        if idx2 > idx1
+          pairs << [ ele1, ele2 ]
+        end
+      end
+    end
+  
+    return pairs
+  end
+  
+  print combinations(["a", "b", "c"]); # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ]
+  puts
+  
+  print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
+  puts
