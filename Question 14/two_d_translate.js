441771d12055a99b0d2b3288e16350eb27aaf837
Nnaji Victor <37231697+Nnaji-Victor@users.noreply.github.com>
2019-03-23 04:11:40 +0100

    Update two_d_translate.js
    This is precise and more concise.

--- Question 14/two_d_translate.js
+++ Question 14/two_d_translate.js
@@ -1,27 +1,13 @@
 function two_d_translate(arr){
-    let newArr= '';
-    let array =[];
-  
-    for(let i =0; i< arr.length; i++){
-      for(let j=0; j< arr[i].length; j = j+2){
-        let ele = arr[i][j];
-        let num = arr[i][j+1];
-  
-        if (num === 0){
-          continue;
-        }
-        array[i] = Array(num).fill(ele);
-      }
-      
-    }
-    newArr = array.toString().split(',');
-    return newArr;
-  }
-  
-  arr_1 = [
-    ['boot', 3],
-    ['camp', 2],
-    ['program', 0]
-  ]
-  
-  console.log(two_d_translate(arr_1));
+  return arr.reduce((result, [ele, num]) =>
+    result.concat(Array(num).fill(ele)),
+  []);
+}
+
+arr_1 = [
+  ['boot', 3],
+  ['camp', 2],
+  ['program', 0]
+]
+
+console.log(two_d_translate(arr_1));
