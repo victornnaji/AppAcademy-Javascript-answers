8a3e40405e738f975515aae8dbf815389b16fccc
Nnaji Victor <nnajivictor0@gmail.com>
2019-03-28 19:05:51 +0100

    answer prime

---
+++ Advanced Problems/Question 8/prime.js
@@ -0,0 +1,14 @@
+function prime(num){
+    if(num <= 1){
+      return false;
+    }
+  
+    for(let i = 2; i < num; i++){
+      if(num % i === 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(prime(6))
