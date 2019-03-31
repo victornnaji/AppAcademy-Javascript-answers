7130b12eb3ec0df5ad1bacb180af637a8273e732
Nnaji Victor <37231697+Nnaji-Victor@users.noreply.github.com>
2019-03-31 18:15:59 +0100

    piglatin

--- Arrays/Question 10/pig_latin_word.js
+++ Arrays/Question 10/pig_latin_word.js
@@ -1,32 +1,21 @@
 function pig_latin_word(str){
-
-    let strArr = '';
-    let vowel = 'aeiou'.split('');
-    if (vowel.includes(str[0])){
-      return str + 'yay'
-    }
-  
-    let vowelPos = check(str, vowel);
-  
-     strArr = str.split('');
-    while (vowelPos > 0){
-      let ele = strArr.shift();
-      strArr.push(ele);
-  
-      vowelPos--;
-    }
-  
-    let answer = strArr.join('');
-    return answer + 'ay';
-  
+    let strArr = [];
+    let tempChar;
     
-  }
-  
-  function check(str,vowel){
-    for(let i =0; i< vowel.length; i++){
-      let firstVowel = str.indexOf(vowel[i]);
-      return firstVowel;
+    function isConsonant(char){
+        return !/[aeiou]/.test(char);
     }
+    
+    if(!isConsonant(str.charAt(0)))
+        return str + 'Way';
+    else
+      strArr = str.split('');
+
+      while(isConsonant(strArr[0])){
+        tempChar = strArr.shift();
+        strArr.push(tempChar);
+      }
+      return strArr.join('') + 'Ay'
   }
   
-  console.log(pig_latin_word("eat"))
+  console.log(pig_latin_word("cyeat"))
